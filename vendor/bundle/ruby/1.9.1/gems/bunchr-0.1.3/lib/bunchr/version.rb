2a680a94b2823cca76ecbc1206997bf03aff241e
AJ Christensen <aj@junglist.gen.nz>
2012-06-21 12:09:19 +1200

    [bundler] bundle package and deployment

---
+++ vendor/bundle/ruby/1.9.1/gems/bunchr-0.1.3/lib/bunchr/version.rb
@@ -0,0 +1,3 @@
+module Bunchr
+  VERSION = '0.1.3'
+end
---
+++ vendor/bundle/ruby/1.9.1/gems/fpm-0.4.10/lib/fpm/package/dir.rb
@@ -0,0 +1,133 @@
+require "fpm/package"
+require "backports"
+require "fileutils"
+require "find"
+require "socket"
+
+# A directory package.
+#
+# This class supports both input and output. As a note, 'output' will
+# only emit the files, not any metadata. This is an effective way
+# to extract another package type.
+class FPM::Package::Dir < FPM::Package
+  private
+
+  # Add a new path to this package.
+  #
+  # If the path is a directory, it is copied recursively. The behavior
+  # of the copying is modified by the :chdir and :prefix attributes.
+  #
+  # If :prefix is set, the destination path is prefixed with that value.
+  # If :chdir is set, the current directory is changed to that value
+  # during the copy.
+  #
+  # Example: Copy /etc/X11 into this package as /opt/xorg/X11:
+  #
+  #     package.attributes[:prefix] = "/opt/xorg"
+  #     package.attributes[:chdir] = "/etc"
+  #     package.input("X11")
+  def input(path)
+    @logger.debug("Copying", :input => path)
+    @logger["method"] = "input"
+    ::Dir.chdir(@attributes[:chdir] || ".") do
+      if @attributes[:prefix]
+        clone(path, File.join(staging_path, @attributes[:prefix]))
+      else
+        clone(path, staging_path)
+      end
+    end
+
+    # Set some defaults. This is useful because other package types
+    # can include license data from themselves (rpms, gems, etc),
+    # but to make sure a simple dir -> rpm works without having
+    # to specify a license.
+    self.license = "unknown"
+    self.vendor = [ENV["USER"], Socket.gethostname].join("@")
+  ensure
+    # Clean up any logger context we added.
+    @logger.remove("method")
+  end # def input
+
+  # Output this package to the given directory.
+  def output(dir)
+    dir = File.expand_path(dir)
+    ::Dir.chdir(staging_path) do
+      @logger["method"] = "output"
+      clone(".", dir)
+    end
+  ensure
+    @logger.remove("method")
+  end # def output
+
+  private
+  # Copy a file or directory to a destination
+  #
+  # This is special because it respects the full path of the source.
+  # Aditionally, hardlinks will be used instead of copies.
+  #
+  # Example:
+  #
+  #     clone("/tmp/hello/world", "/tmp/example")
+  #
+  # The above will copy, recursively, /tmp/hello/world into
+  # /tmp/example/hello/world
+  def clone(source, destination)
+    # Copy all files from 'path' into staging_path
+
+    Find.find(source) do |path|
+      target = File.join(destination, path)
+      copy(path, target)
+    end
+  end # def clone
+
+  # Copy a path.
+  #
+  # Files will be hardlinked if possible, but copied otherwise.
+  # Symlinks should be copied as symlinks.
+  def copy(source, destination)
+    directory = File.dirname(destination)
+    if !File.directory?(directory)
+      FileUtils.mkdir_p(directory)
+    end
+
+    # Create a directory if this path is a directory
+    if File.directory?(source) and !File.symlink?(source)
+      @logger.debug("Creating", :directory => destination)
+      if !File.directory?(destination)
+        FileUtils.mkdir(destination)
+      end
+    else
+      # Otherwise try copying the file.
+      begin
+        @logger.debug("Linking", :source => source, :destination => destination)
+        File.link(source, destination)
+      rescue Errno::EXDEV
+        # Hardlink attempt failed, copy it instead
+        @logger.debug("Copying", :source => source, :destination => destination)
+        FileUtils.copy_entry(source, destination)
+      end
+    end
+
+    copy_metadata(source, destination)
+  end # def copy
+
+  def copy_metadata(source, destination)
+    source_stat = File::lstat(source)
+    dest_stat = File::lstat(destination)
+
+    # If this is a hard-link, there's no metadata to copy.
+    return if source_stat.ino == dest_stat.ino
+
+    File.utime(source_stat.atime, source_stat.mtime, destination)
+    begin
+      File.chown(source_stat.uid, source_stat.gid, destination)
+    rescue Errno::EPERM
+      # clear setuid/setgid
+      File.chmod(source_stat.mode & 01777, destination)
+    else
+      File.chmod(source_stat.mode, destination)
+    end
+  end # def copy_metadata
+
+  public(:input, :output)
+end # class FPM::Package::Dir
